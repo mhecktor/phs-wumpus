2c1e7c3cc415972bf1a19967180919d73cb784e1
Marco Hecktor <marco.hecktor@com2m.de>
2017-12-07 16:44:32 +0100

    formatting and added WumpusFieldType

---
+++ src/de/fh/Model/WumpusFieldType.java
@@ -0,0 +1,11 @@
+package de.fh.Model;
+
+public enum WumpusFieldType {
+    BREEZE,
+    HOLE,
+    GOLD,
+    PLAYER,
+    STENCH,
+    WALL,
+    WUMPUS,
+}
--- src/de/fh/MyWumpusAgent.java
+++ src/de/fh/MyWumpusAgent.java
@@ -4,50 +4,50 @@ import de.fh.agent.WumpusHunterAgent;
 import de.fh.wumpus.HunterPercept;
 import de.fh.wumpus.enums.HunterAction;
 import de.fh.wumpus.enums.HunterActionEffect;
+
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
 
-/*
- * DIESE KLASSE VERÄNDERN SIE BITTE NUR AN DEN GEKENNZEICHNETEN STELLEN
- * wenn die Bonusaufgabe bewertet werden soll.
- */
 public class MyWumpusAgent extends WumpusHunterAgent {
 
-	private HunterPercept percept;
-	private HunterActionEffect actionEffect;
-	private Hashtable<Integer, Integer> stenchRadar;
-	
-	public static void main(String[] args) {
+    private HunterPercept percept;
+    private HunterActionEffect actionEffect;
+    private Hashtable<Integer, Integer> stenchRadar;
 
-		MyWumpusAgent agent = new MyWumpusAgent("");
-		MyWumpusAgent.start(agent,"127.0.0.1", 5000);
-	}
+    private ArrayList<ArrayList<Integer>> wumpusMap = new ArrayList<ArrayList<Integer>>();
 
-	public MyWumpusAgent(String name) {
+    public static void main(String[] args) {
 
-		super(name);
-	}
+        MyWumpusAgent agent = new MyWumpusAgent("");
+        MyWumpusAgent.start(agent, "127.0.0.1", 5000);
+    }
 
-	/**
-	 * In dieser Methode kann das Wissen über die Welt (der State, der Zustand)
-	 * entsprechend der aktuellen Wahrnehmungen anpasst, und die "interne Welt",
-	 * die Wissensbasis, des Agenten kontinuierlich ausgebaut werden.
-	 *
-	 * Wichtig: Diese Methode wird aufgerufen, bevor der Agent handelt, d.h.
-	 * bevor die action()-Methode aufgerufen wird...
-	 *
-	 * @param percept Aktuelle Wahrnehmung
-	 * @param actionEffect Reaktion des Servers auf vorhergewählte Aktion
-	 */
-	@Override
-	public void updateState(HunterPercept percept, HunterActionEffect actionEffect) {
+    public MyWumpusAgent(String name) {
 
-		/**
-		 * Je nach Sichtbarkeit & Schwierigkeitsgrad (laut Serverkonfiguration)
-		 * aktuelle Wahrnehmung des Hunters.
-		 * Beim Wumpus erhalten Sie je nach Level mehr oder weniger Mapinformationen.
-		 */
-		this.percept = (HunterPercept) percept;
+        super(name);
+    }
+
+    /**
+     * In dieser Methode kann das Wissen über die Welt (der State, der Zustand)
+     * entsprechend der aktuellen Wahrnehmungen anpasst, und die "interne Welt",
+     * die Wissensbasis, des Agenten kontinuierlich ausgebaut werden.
+     * <p>
+     * Wichtig: Diese Methode wird aufgerufen, bevor der Agent handelt, d.h.
+     * bevor die action()-Methode aufgerufen wird...
+     *
+     * @param percept      Aktuelle Wahrnehmung
+     * @param actionEffect Reaktion des Servers auf vorhergewählte Aktion
+     */
+    @Override
+    public void updateState(HunterPercept percept, HunterActionEffect actionEffect) {
+
+        /**
+         * Je nach Sichtbarkeit & Schwierigkeitsgrad (laut Serverkonfiguration)
+         * aktuelle Wahrnehmung des Hunters.
+         * Beim Wumpus erhalten Sie je nach Level mehr oder weniger Mapinformationen.
+         */
+        this.percept = percept;
 
         /*
          Aktuelle Reaktion des Server auf die letzte übermittelte Action.
@@ -87,12 +87,12 @@ public class MyWumpusAgent extends WumpusHunterAgent {
          }
          */
 
-		System.out.println("-------------------------------");
-		this.actionEffect = actionEffect;
-		System.out.println(actionEffect.toString()+ "\n");
+        System.out.println("-------------------------------");
+        this.actionEffect = actionEffect;
+        System.out.println(actionEffect.toString() + "\n");
 
 		/*
-		Mögliche Percepts über die Welt erhält der Wumpushunter:
+        Mögliche Percepts über die Welt erhält der Wumpushunter:
 
 		percept.isBreeze();
         percept.isBump();
@@ -109,37 +109,36 @@ public class MyWumpusAgent extends WumpusHunterAgent {
         Die Manhattendistanz zum jeweiligen Wumpi ergibt sich aus der Gestanksitensität (getValue).
 		*/
 
-		//Beispiel:
-		stenchRadar = this.percept.getWumpusStenchRadar();
+        //Beispiel:
+        stenchRadar = this.percept.getWumpusStenchRadar();
 
-		//Gebe alle riechbaren Wumpis aus
-		System.out.println("WumpusID: Intensitaet");
-		if(stenchRadar.isEmpty())
-		{
-			System.out.println("Kein Wumpi zu riechen");
-		}
-		for(Map.Entry<Integer, Integer> g : stenchRadar.entrySet()){
-			System.out.println(g.getKey() + ":\t\t" + g.getValue() );
-		}
-		System.out.println("");
+        //Gebe alle riechbaren Wumpis aus
+        System.out.println("WumpusID: Intensitaet");
+        if (stenchRadar.isEmpty()) {
+            System.out.println("Kein Wumpi zu riechen");
+        }
+        for (Map.Entry<Integer, Integer> g : stenchRadar.entrySet()) {
+            System.out.println(g.getKey() + ":\t\t" + g.getValue());
+        }
+        System.out.println("");
 
 
-	}
+    }
 
-	/**
-	 * Diesen Part erweitern Sie so, dass die nächste(n) sinnvolle(n) Aktion(en),
-	 * auf Basis der vorhandenen Zustandsinformationen und gegebenen Zielen, ausgeführt wird/werden.
-	 * Der action-Part soll den Agenten so intelligent wie möglich handeln lassen
-	 *
-	 * Beispiel: Wenn die letzte Wahrnehmung
-	 * "percept.isGlitter() == true" enthielt, ist "HunterAction.GRAB" eine
-	 * geeignete Tätigkeit. Wenn Sie wissen, dass ein Quadrat "unsicher"
-	 * ist, können Sie wegziehen
-	 *
-	 * @return Die nächste HunterAction die vom Server ausgeführt werden soll
-	 */
-	@Override
-	public HunterAction action() {
+    /**
+     * Diesen Part erweitern Sie so, dass die nächste(n) sinnvolle(n) Aktion(en),
+     * auf Basis der vorhandenen Zustandsinformationen und gegebenen Zielen, ausgeführt wird/werden.
+     * Der action-Part soll den Agenten so intelligent wie möglich handeln lassen
+     * <p>
+     * Beispiel: Wenn die letzte Wahrnehmung
+     * "percept.isGlitter() == true" enthielt, ist "HunterAction.GRAB" eine
+     * geeignete Tätigkeit. Wenn Sie wissen, dass ein Quadrat "unsicher"
+     * ist, können Sie wegziehen
+     *
+     * @return Die nächste HunterAction die vom Server ausgeführt werden soll
+     */
+    @Override
+    public HunterAction action() {
 
 		/*HunterAction
         Mögliche HunterActions sind möglich:
@@ -153,8 +152,8 @@ public class MyWumpusAgent extends WumpusHunterAgent {
 		HunterAction.QUIT_GAME
 		*/
 
-		nextAction = HunterAction.GO_FORWARD;
-		System.out.println("nextAction: "+nextAction);
-		return nextAction;
-	}
+        nextAction = HunterAction.GO_FORWARD;
+        System.out.println("nextAction: " + nextAction);
+        return nextAction;
+    }
 }
